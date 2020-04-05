3dba7568a33431b183ed88ff5d6675453fd44363
Veerash Palanichamy <veerashp05@gmail.com>
2020-04-04 21:12:36 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+readTextFile();
+function addCounter(){
+    var counter = document.getElementById("counter");
+    counter.textContent = parseInt(counter.textContent)+1;
+}
+
+function readTextFile() {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "./data.txt", true);
+    rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4) {
+        var allText = rawFile.responseText;
+        console.log("allText")
+      }
+    }
+    rawFile.send(null);
+  }
